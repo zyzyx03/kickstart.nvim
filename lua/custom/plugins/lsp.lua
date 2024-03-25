9f2d93b5e34f9ab94d6333110bc3fa7a0a132a3d
zyzyx03 <fauzuwan.nazri93@gmail.com>
2024-03-26 06:50:09 +0800

    feat: add  lsp.lua

---
+++ lua/custom/plugins/lsp.lua
@@ -0,0 +1,17 @@
+return {
+  -- tools
+  {
+    'williamboman/mason.nvim',
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        'selene',
+        'luacheck',
+        'shellcheck',
+        'shfmt',
+        'tailwindcss-language-server',
+        'typescript-language-server',
+        'css-lsp',
+      })
+    end,
+  },
+}
