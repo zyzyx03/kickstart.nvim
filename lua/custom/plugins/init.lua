74a7ee86fca8d327528a2a64cd43f9ff6f1694c6
fauzuwan <fyzyx@pop-os.localdomain>
2024-03-26 00:39:44 +0800

    feat: add nvim neo tree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,18 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+-- File: lua/custom/plugins/filetree.lua
+
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    version = "*",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    config = function ()
+      require('neo-tree').setup {}
+    end,
+  }
